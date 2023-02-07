9907d272e4c4915a4c43dc0ae541667bd3fa30df
Sergei Vorobyov <Sergei.Vorobyov@solita.se>
2023-02-07 20:42:11 +0100

    test secret, 1

--- python/main.py
+++ python/main.py
@@ -1,2 +1,5 @@
+CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=sv-cosmos-01-tbl;AccountKey=3HMiw9mqsgguJ56vMAZ8tq9oO0hxSzUxfQwd5Hj0TZtPst6OshQEPCIKiks7MIS9N4qJXFIz4aHYlkuu8KgzkA==;TableEndpoint=https://sv-cosmos-01-tbl.table.cosmos.azure.com:443/;"
+
+
 if __name__ == '__main__':
     pass
