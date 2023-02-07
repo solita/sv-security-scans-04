9776bcb586bb1afc33b9db067382a0e4a4de0cb5
Sergei Vorobyov <Sergei.Vorobyov@solita.se>
2023-02-07 20:49:34 +0100

    test secret, AWS, 2

--- python/main.py
+++ python/main.py
@@ -1,4 +1,5 @@
-CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=sv-cosmos-01-tbl;AccountKey=3HMiw9mqsgguJ56vMAZ8tq9oO0hxSzUxfQwd5Hj0TZtPst6OshQEPCIKiks7MIS9N4qJXFIz4aHYlkuu8KgzkA==;TableEndpoint=https://sv-cosmos-01-tbl.table.cosmos.azure.com:443/;"
+# 2023-02-07: GitHub does NOT detect this secret!
+CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=sv-cosmos-01-tbl;AccountKey=3HMiw9mqsgguJ56vMAZ8tq9oO0hxSzUxfQwd5Hj0TZtPst6OshQEPCIAbba7MIS9N4qJXFIz4aHYlkuu8KgzkA==;TableEndpoint=https://foo-bar-01-tbl.table.cosmos.azure.com:443/;"
 
 
 if __name__ == '__main__':
